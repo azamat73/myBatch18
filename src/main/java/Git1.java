ccad3fbfb0a6c2504a441b57acf82ebb4df15de2
azamat73 <kaazamat73@gmail.com>
2023-12-07 14:20:48 -0600

    br1

--- src/main/java/Git1.java
+++ src/main/java/Git1.java
@@ -2,6 +2,9 @@ public class Git1 {
     public static void main(String[] args) {
         System.out.println("Class 1");
         //first change after git init
+        //Third changes after git init
+        //Third change
+        //change from branch number 1
 
     }
 }
